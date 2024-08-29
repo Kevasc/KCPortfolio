efe8a3370f871cebdcdc33bd35f47430e7e20851
kevas cole <colekevas@gmail.com>
2024-08-29 10:36:35 +0100

    removed unnecessary text

--- my-app/src/components/body/BodyLowerSection.jsx
+++ my-app/src/components/body/BodyLowerSection.jsx
@@ -8,16 +8,6 @@ const BodyLowerSection = () => {
           Featured
           <br /> Projects
         </p>
-        <p className="lower-column-text">
-          If you’re already a lover of Neapolitan pizza then you’ll know what
-          we’re talking about – the taste, the texture, the pillowy crust, the
-          chew. There is nothing like it and once you’ve experienced Neapolitan
-          pizza it’s difficult to enjoy any other.
-          <br /> <br />
-          Neapolitan wood fired pizzas are an art, something we passionately
-          imitate and something we celebrate – it’s what makes our difference
-          and means you get the authentic taste experience.
-        </p>
       </div>
     </div>
   );
