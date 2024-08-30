2fc09183be1777d017824d71d8c1881e3af0aedc
kevas cole <colekevas@gmail.com>
2024-08-30 14:57:11 +0100

    wrote a description for the star seeker project and renamed class names

--- my-app/src/components/body/StarSeekerProject.jsx
+++ my-app/src/components/body/StarSeekerProject.jsx
@@ -1,41 +1,36 @@
 import React from "react";
 import "./StarSeekerProject.css";
+
 const StarSeekerProject = () => {
   return (
-    <div id="projects" className="projects-section">
-      <div className="projects-left-column">
-        <a
-          href="https://github.com/Kevasc/confetti-form"
-          className="confetti-screenshot"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={require("../../assets/confettiScreenshot.png")}
-            alt="Confetti Screenshot"
-            className="confetti-screenshot"
-          />
-
-          <div className="confetti-screenshot"></div>
-        </a>
-      </div>
-      <div className="projects-right-column">
-        <div className="projects-column-content">
-          <h1 className="projects-column-title">Confetti Form</h1>
-          <p className="projects-column-text">
-            This project contains an HTML form that collects user information
-            including first name, last name, email, and date of birth. When the
-            form is submitted, it checks the last digit of the birth year and
-            launches a confetti animation if the year ends in "5" or "0". If the
-            year does not end in "5" or "0", it shows an alert message.
-            <ul>
-              <li>HTML</li>
-              <li>CSS </li>
-              <li>Javascript </li>
-              <li>Bootstrap for form styling</li>
-              <li>animation powered by canvas-confetti</li>
-            </ul>
+    <div id="projects" className="starseeker-section">
+      <div className="starseeker-left-column">
+        <div className="starseeker-column-width">
+          <h1 className="starseeker-column-title">Star Seeker</h1>
+          <p className="starseeker-column-text">
+            Star Seeker is a user-friendly application designed to help you
+            explore the cosmos effortlessly. Whether you're planning
+            interstellar journeys, calculating travel costs, or searching for
+            the most affordable routes, Star Seeker provides the tools you need.
+            The app features a sleek design and seamless functionality, making
+            it your ideal companion for cosmic adventures.
+            <br />
+            <br></br>
+            <h2>Built With:</h2>
+            React | Redux | CSS | HTML
           </p>
+        </div>
+      </div>
+      <div className="starseeker-right-column">
+        <div className="starseeker-screenshot">
+          <a
+            href="https://github.com/Kevasc/star-seeker"
+            className="github-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""}
+          </a>
         </div>
       </div>
     </div>
