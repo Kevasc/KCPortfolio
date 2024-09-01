7b758285c2896dade988ee51de209aea14e19c1b
kevas cole <colekevas@gmail.com>
2024-09-01 20:35:18 +0100

    changed logo to the blue, added on:hover effect to the nav and added underline

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import logo from "../assets/kevaslogo.png";
+import logo from "../assets/kcbluelogo.png";
 const Header = () => {
   const scrollToSection = (id) => {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
