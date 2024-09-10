e2cc07d9fb6000df5973a2e2879c23e950635033
kevas cole <colekevas@gmail.com>
2024-09-10 10:50:45 +0100

    reformatted the introduction text

--- src/components/body/IntroductionSection.jsx
+++ src/components/body/IntroductionSection.jsx
@@ -11,13 +11,14 @@ const IntroductionSection = () => {
           <p className="introduction-column-text">
             After nearly a decade of refining my skills in the fast-paced world
             of hospitality, I am embarking on an exciting career switch into
-            front-end technology. My attention to detail, strong communication
-            abilities, and dedication to creating memorable experiences are now
-            being channeled into crafting seamless, user-friendly web
-            interfaces. Passionate about design and driven by a love for
-            learning, I am ready to combine my hospitality background with my
-            new technical expertise to create digital experiences that truly
-            resonate with users.
+            front-end technology. <br />
+            My attention to detail, strong communication abilities, and
+            dedication to creating memorable experiences are now being channeled
+            into crafting seamless, user-friendly web interfaces.
+            <br /> Passionate about design and driven by a love for learning, I
+            am ready to combine my hospitality background with my new technical
+            expertise to create digital experiences that truly resonate with
+            users.
           </p>
         </div>
       </div>
