9bf52e714a291cd5435f39eaff9f7ea6c3890b7e
kevas cole <colekevas@gmail.com>
2025-02-10 17:54:37 +0000

    changed background colour, changed text size, changed font to monospace, deleted main logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import logo from "../assets/kcbluelogo.png";
+
 const Header = () => {
   const scrollToSection = (id) => {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
@@ -8,7 +8,6 @@ const Header = () => {
 
   return (
     <div className="heading">
-      <img className="logo" src={logo} alt="logo" />
       <div className="header-button-container">
         <div
           className="header-button"
@@ -16,18 +15,17 @@ const Header = () => {
         >
           <p>ABOUT ME</p>
         </div>
-
-        <div
-          className="header-button"
-          onClick={() => scrollToSection("projects")}
-        >
-          <p>FEATURED PROJECTS</p>
-        </div>
         <div
           className="header-button"
           onClick={() => scrollToSection("languages")}
         >
           <p>LANGUAGES & FRAMEWORKS</p>
+        </div>
+        <div
+          className="header-button"
+          onClick={() => scrollToSection("projects")}
+        >
+          <p>FEATURED PROJECTS</p>
         </div>
         <div
           className="header-button"
