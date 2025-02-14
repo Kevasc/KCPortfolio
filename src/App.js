eb98a4f45b8f8c8017ce013434fedf8dac75dcdb
kevas cole <colekevas@gmail.com>
2025-02-14 15:00:00 +0000

    Added so hopefully app will deploy onto vercel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import BodyContainer from "./components/body/BodyContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+<link rel="manifest" href="/manifest.json" />;
+
 function App() {
   return (
     <div className="App">
